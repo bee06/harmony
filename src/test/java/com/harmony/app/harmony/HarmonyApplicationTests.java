946b408cf297ff612cf2e2a28581a87a5d7465a1
jh <zong.jianhui06@gmail.com>
2022-01-07 17:42:43 +0800

    init

---
+++ src/test/java/com/harmony/app/harmony/HarmonyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harmony.app.harmony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarmonyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
